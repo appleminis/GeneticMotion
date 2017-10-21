fd0388a8e1f3f00d3c1e0896c3e5a64bf11678ce
julien.rodriguez-tao <julien.rodriguez-tao@devisubox.com>
2017-10-21 16:39:10 +0200

    first commit

---
+++ cellmanager.cpp
@@ -0,0 +1,6 @@
+#include "cellmanager.h"
+
+CellManager::CellManager()
+{
+
+}
---
+++ cellmanager.h
@@ -0,0 +1,12 @@
+#ifndef CELLMANAGER_H
+#define CELLMANAGER_H
+
+#include "cell.h"
+
+class CellManager
+{
+public:
+    CellManager();
+};
+
+#endif // CELLMANAGER_H
